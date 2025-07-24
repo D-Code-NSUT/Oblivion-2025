98fc9a8d7ea8e33e566028883846912474bf1295
Kartik991 <kartikx5126@gmail.com>
2025-07-24 17:50:41 +0530

    an animation added to  section, about section fade-in not fixed yet

--- src/components/home-client.tsx
+++ src/components/home-client.tsx
@@ -5,7 +5,7 @@ import styles from '@/app/page.module.css';
 import Community from '@/components/community/community';
 import Beams from '@/modules/Beams/Beams';
 import { useHashScroll } from '@/hooks/use-hash-scroll';
-import About from '@/components/home/About.section';
+import About from '@/components/home/about';
 import TimeLine from '@/components/timeline/timeline';
 import JoinCommunity from '@/components/join-community/join-community';
 
@@ -18,9 +18,9 @@ export default function HomeClient() {
       <Hero />
       
       <section id="features" className={`${styles.section} ${styles.section3}`}>
-        <div>
+      
           <About />
-        </div>
+        
       </section>
 
 <TimeLine />
