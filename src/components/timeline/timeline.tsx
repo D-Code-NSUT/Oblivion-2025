71e2afb9aaa673141dff8e2ccfe375e6e1bb4f21
Roshan <roshan.sharmaa12@gmail.com>
2025-07-30 00:14:48 +0530

    adds some spacing between timeline texts

--- src/components/timeline/timeline.tsx
+++ src/components/timeline/timeline.tsx
@@ -32,7 +32,7 @@ const TimeLine = () => {
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 2, delay: 0 }}
             viewport={{ once: true }}
-            className="absolute md:top-[55%] top-[54%] md:left-[17%] left-[17%] text-[#5CE4FF] font-roboto font-bold lg:text-[30px] md:text-[22px] sm:text-[20px] xs:text-[16px] text-[14px]"
+            className="absolute md:top-[55%] top-[54%] md:left-[18%] left-[17%] text-[#5CE4FF] font-roboto font-bold lg:text-[30px] md:text-[22px] sm:text-[20px] xs:text-[16px] text-[14px]"
           >
             29th August 2025
           </motion.h2>
@@ -41,7 +41,7 @@ const TimeLine = () => {
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 2.2, delay: 0 }}
             viewport={{ once: true }}
-            className="absolute md:top-[58%] top-[57%] md:left-[17%] left-[17%] text-[#FFA02B] font-bebas lg:text-[55px] md:text-[45px] sm:text-[40px] xs:text-[28px] text-[26px]"
+            className="absolute md:top-[59%] top-[57%] md:left-[18%] left-[17%] text-[#FFA02B] font-bebas lg:text-[55px] md:text-[45px] sm:text-[40px] xs:text-[28px] text-[26px]"
           >
             DAY1: EMBARK
           </motion.h2>
@@ -51,7 +51,7 @@ const TimeLine = () => {
             whileInView={{ opacity: 1, y: 30 }}
             transition={{ duration: 2, delay: 1 }}
             viewport={{ once: true }}
-            className="absolute md:top-[40%] top-[42%] md:left-[36%] left-[40%] text-[#5CE4FF] font-roboto font-bold lg:text-[30px]  md:text-[22px] sm:text-[20px] xs:text-[16px] text-[14px]"
+            className="absolute md:top-[38%] top-[42%] md:left-[37%] left-[40%] text-[#5CE4FF] font-roboto font-bold lg:text-[30px]  md:text-[22px] sm:text-[20px] xs:text-[16px] text-[14px]"
           >
             30th August 2025
           </motion.h2>
@@ -60,7 +60,7 @@ const TimeLine = () => {
             whileInView={{ opacity: 1, y: 30 }}
             transition={{ duration: 2.2, delay: 1 }}
             viewport={{ once: true }}
-            className="absolute md:top-[34%] top-[38%] md:left-[36%] left-[40%] text-[#FFA02B] font-bebas lg:text-[55px] md:text-[45px] sm:text-[40px] xs:text-[28px] text-[26px]"
+            className="absolute md:top-[31%] top-[38%] md:left-[37%] left-[40%] text-[#FFA02B] font-bebas lg:text-[55px] md:text-[45px] sm:text-[40px] xs:text-[28px] text-[26px]"
           >
             DAY2: NAVIGATE
           </motion.h2>
@@ -70,7 +70,7 @@ const TimeLine = () => {
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 2, delay: 1.5 }}
             viewport={{ once: true }}
-            className="absolute md:top-[55%] top-[54%] md:left-[58%] left-[65%] text-[#5CE4FF] font-roboto font-bold lg:text-[30px]  md:text-[22px] sm:text-[20px] xs:text-[16px] text-[14px]"
+            className="absolute md:top-[55%] top-[54%] md:left-[59%] left-[65%] text-[#5CE4FF] font-roboto font-bold lg:text-[30px]  md:text-[22px] sm:text-[20px] xs:text-[16px] text-[14px]"
           >
             31st August 2025
           </motion.h2>
@@ -79,7 +79,7 @@ const TimeLine = () => {
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 2.2, delay: 1.5 }}
             viewport={{ once: true }}
-            className="absolute md:top-[58%] top-[57%] md:left-[58%] left-[65%] text-[#FFA02B] font-bebas lg:text-[55px] md:text-[45px] sm:text-[40px] xs:text-[28px] text-[26px]"
+            className="absolute md:top-[59%] top-[57%] md:left-[59%] left-[65%] text-[#FFA02B] font-bebas lg:text-[55px] md:text-[45px] sm:text-[40px] xs:text-[28px] text-[26px]"
           >
             DAY3: conquer
           </motion.h2>
