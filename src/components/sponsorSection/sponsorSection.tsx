4abc16762344bce898428e51cf6c3697b8b65e7b
Ayush rawat <165577927+Ayu-Rawat@users.noreply.github.com>
2025-08-29 00:06:55 +0530

    Update sponsor data and improve SponsorCard component styling

--- src/components/sponsorSection/sponsorSection.tsx
+++ src/components/sponsorSection/sponsorSection.tsx
@@ -1,44 +1,35 @@
-import React from 'react'
-import { SponsorData, sponsorsData} from '../../data/sponsorData/homeSponsorsData'
-import Image from 'next/image'
-import styles from '@/components/sponsorSection/sponsorSection.module.css'
+import React from 'react';
+import { SponsorData, sponsorsData } from '../../data/sponsorData/homeSponsorsData';
+import Image from 'next/image';
+import styles from '@/components/sponsorSection/sponsorSection.module.css';
 import { motion } from 'motion/react';
-function sponorCard(sponsorData:SponsorData,index:number,priority:number){
-    return(
-    <div className={` ${
-        priority==1? 'my-4 w-[34vw]': 
-        priority==2? 'my-4 w-[26vw]':
-        priority==3? 'my-3 w-[20vw]':'my-3 w-[15vw]'
-    }`} key={index}>
-        <div className={`bg-cyan-900/30 pb-2 transiton drop-shadow-[0_0_15px_rgba(34,211,238,0.5)] hover:drop-shadow-[0_0_15px_rgba(34,211,238,0.75)] duration-400 flex flex-col h-full relative justify-between ${
-            priority==1?'w-[30vw] md:w-[25vw] lg:w-[25vw] ':
-            priority==2?'w-[23vw] md:w-[20vw] lg:w-[20vw]':
-            priority==3?'min-w-[15vw] md:min-w-[15vw] max-w-[85%] lg:min-w-[15vw]  pb-3 pt-5':'min-w-[80%] max-w-[85%] md:px-2 lg:px-3 pb-3 pt-5'} overflow-hidden  mx-auto rounded-3xl border-cyan-700 border-3 items-center`}>    
-            
-            {/* <Image
-                src="/sponsiBcg.webp"
-                alt="Decorative background"
-                fill
-                className="opacity-50 object-contain"
-                quality={75}
-            /> */}
-            
-            { priority<3 && <div className={`text-sm ${styles['pirata-one-regular']}
-              drop-shadow-[0_0_12px_rgba(34,211,238,0.3)] whitespace-normal  tracking-wide font-light md:text-2xl lg:text-2xl 
-              z-10 w-full lg:w-fit text-center mb-3 px-1 md:px-4 lg:px-4 py-1 border-b-3 border-cyan-700
-               md:border-3 md:border-t-0 lg:border-3 lg:border-t-0 bg-[rgba(23,75,90,0.7)] ${'md:rounded-b-xl'} lg:rounded-b-xl`}>{sponsorData.title||"Title Sponsor"}</div>}
-            <Image  className='sponser z-10 w-[90%] mx-auto  md:w-[70%] h-auto lg:w-[50%] drop-shadow-[0_0_10px_rgba(0,0,0,0.8)]' src={sponsorData.image} alt={sponsorData.name||""} quality={100} width={800} height={800}/>
-            {/*<div className='w-[90%] my-1 text-center name px-1 z-10 '>{sponsorData.name}</div>*/}
+
+function SponsorCard(sponsorData: SponsorData, index: number, priority: number) {
+    return (
+        <div className="m-2" key={index}>
+            <div className="bg-cyan-900/30 pb-2 transition drop-shadow-[0_0_15px_rgba(34,211,238,0.5)] hover:drop-shadow-[0_0_15px_rgba(34,211,238,0.75)] duration-400 flex flex-col relative justify-between items-center overflow-hidden w-[60vw] md:w-[25vw] lg:w-[22vw] h-[260px] lg:h-[260px] mx-auto rounded-3xl border-cyan-700 border-3">
+                <div className={`text-sm ${styles['pirata-one-regular']} drop-shadow-[0_0_12px_rgba(34,211,238,0.3)] whitespace-normal tracking-wide font-light md:text-2xl lg:text-2xl z-10 w-full lg:w-fit text-center px-1 md:px-4 lg:px-4 py-1 border-b-3 border-cyan-700 md:border-3 md:border-t-0 lg:border-3 lg:border-t-0 bg-[rgba(23,75,90,0.7)] md:rounded-b-xl lg:rounded-b-xl`}>
+                    {sponsorData.title || 'Sponsor'}
+                </div>
+                <div className="flex-1 flex items-center justify-center w-full">
+                    <Image
+                        className={`sponser z-10 mx-auto h-auto w-[90%] md:w-[70%] lg:w-[50%] drop-shadow-[0_0_10px_rgba(0,0,0,0.8)] ${['DU India', 'AccioJob', 'Yosemite Crew'].includes(sponsorData.name) ? 'p-4' : ''}`}
+                        src={sponsorData.image}
+                        alt={sponsorData.name || ''}
+                        quality={100}
+                        width={800}
+                        height={800}
+                    />
+                </div>
+            </div>
         </div>
-    </div>)
+    );
 }
 
 function SponsorSection() {
-  
     return (
-        <div className={`h-full w-full pt-5  bg-[linear-gradient(to_top,_black_0%,_rgba(0,0,0,0)_20%,_rgba(0,0,0,0)_100%)] lg:pb-40 pb-20 lg:bg-[length:100%_auto]`}>
-            
-            <Image 
+        <div className="h-full w-full pt-5 bg-[linear-gradient(to_top,_black_0%,_rgba(0,0,0,0)_20%,_rgba(0,0,0,0)_100%)] lg:pb-40 pb-20 lg:bg-[length:100%_auto]">
+            <Image
                 src="/homeSponsiBcg.webp"
                 alt="Hero Background"
                 fill
@@ -53,54 +44,66 @@ function SponsorSection() {
                 quality={100}
                 priority
             />
-           <motion.div
-            initial={{ opacity: 0, y: 50 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 2 }}
-            viewport={{ once: true }}>
-             <div className={` text-[60px] md:text-[120px] transiton duration-400 hover:drop-shadow-[0_0_20px_rgba(34,211,238,0.4)] text-[#36B9C5] mb-2 md:mb-4 text-center ${styles['pirata-one-regular']}`}>
-                OBLIVION&#39;25 SPONSORS
-            </div>
+            <motion.div
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 2 }}
+                viewport={{ once: true }}
+            >
+                <div className={`text-[60px] md:text-[120px] transition duration-400 hover:drop-shadow-[0_0_20px_rgba(34,211,238,0.4)] text-[#36B9C5] mb-2 md:mb-4 text-center ${styles['pirata-one-regular']}`}>
+                    OBLIVION'25 SPONSORS
+                </div>
             </motion.div>
             <motion.div
-            initial={{ opacity: 0, y: 50 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 2 }}
-            viewport={{ once: true }}>
-            {sponsorsData.top[0] && <div className='flex sm:py-2 lg:py-5 flex-wrap-reverse px-5  items-stretch  justify-center content-around'>
-                {sponsorsData.top.map((sponsorData,index)=>sponorCard(sponsorData,index,1))}
-            </div>}
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 2 }}
+                viewport={{ once: true }}
+            >
+                {sponsorsData.top[0] && (
+                    <div className="flex sm:py-2 lg:py-5 flex-wrap px-5 items-stretch justify-center content-around">
+                        {sponsorsData.top.map((sponsorData, index) => SponsorCard(sponsorData, index, 1))}
+                    </div>
+                )}
             </motion.div>
             <motion.div
-            initial={{ opacity: 0, y: 50 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 2 }}
-            viewport={{ once: true }}>
-            {sponsorsData.second[0] && <div className='flex sm:py-2 lg:py-5 flex-wrap-reverse px-7   items-stretch  justify-center content-around'>
-                {sponsorsData.second.map((sponsorData,index)=>sponorCard(sponsorData,index,2))}
-            </div>}
-            </motion.div>
-             <motion.div
-            initial={{ opacity: 0, y: 50 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 2 }}
-            viewport={{ once: true }}>
-            {sponsorsData.third[0] && <div className='flex sm:py-2 lg:py-5 px-1 flex-wrap-reverse  lg:gap-5 items-stretch  justify-center content-around'>
-                {sponsorsData.third.map((sponsorData,index)=>sponorCard(sponsorData,index,3))}
-            </div>}
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 2 }}
+                viewport={{ once: true }}
+            >
+                {sponsorsData.second[0] && (
+                    <div className="flex sm:py-2 lg:py-5 flex-wrap px-7 items-stretch justify-center content-around">
+                        {sponsorsData.second.map((sponsorData, index) => SponsorCard(sponsorData, index, 2))}
+                    </div>
+                )}
             </motion.div>
             <motion.div
-            initial={{ opacity: 0, y: 50 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 2 }}
-            viewport={{ once: true }}>
-            {sponsorsData.last[0] && <div className='flex sm:py-2 lg:py-5 items-stretch flex-wrap-reverse  lg:gap-5  justify-center content-around'>
-                {sponsorsData.last.map((sponsorData,index)=>sponorCard(sponsorData,index,4))}
-            </div>}
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 2 }}
+                viewport={{ once: true }}
+            >
+                {sponsorsData.third[0] && (
+                    <div className="flex sm:py-2 lg:py-5 px-1 flex-wrap lg:gap-5 items-stretch justify-center content-around">
+                        {sponsorsData.third.map((sponsorData, index) => SponsorCard(sponsorData, index, 3))}
+                    </div>
+                )}
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 2 }}
+                viewport={{ once: true }}
+            >
+                {sponsorsData.last[0] && (
+                    <div className="flex sm:py-2 lg:py-5 items-stretch flex-wrap lg:gap-5 justify-center content-around">
+                        {sponsorsData.last.map((sponsorData, index) => SponsorCard(sponsorData, index, 4))}
+                    </div>
+                )}
             </motion.div>
         </div>
-        
-    )
+    );
 }
 
 export default SponsorSection;
--- src/data/sponsorData/homeSponsorsData.tsx
+++ src/data/sponsorData/homeSponsorsData.tsx
@@ -14,24 +14,56 @@ export interface SponsorData {
 export const sponsorsData = {
   top: [
     {
-      title: "Title Sponser Of Hackathon",
+      title: "Title Sponsor",
+      name: "Red Bull",
+      image: "https://res.cloudinary.com/dqvwf3z2c/image/upload/v1756396632/red-bull_omtpbx.png",
+      link: "https://redbull.com/in-en",
+      custom: "",
+    },
+  ],
+  
+  second: [
+    {
+      title: "Co–Title Sponsor",
       name: "Yosemite Crew",
       image: "/sponserPhoto/yosemite.svg",
       link: "https://openai.com",
       custom: "",
     },
+    {
+      title: "Gold Sponsor",
+      name: "AccioJob",
+      image: "https://res.cloudinary.com/dqvwf3z2c/image/upload/v1756399551/acciojob_logo_s9wpmm.jpg",
+      link: "https://openai.com",
+      custom: "",
+    },
   ],
-
-  second: [
- 
-  ],
-
+  
   third: [
-   
+    {
+      title: "Associate Sponsor",
+      name: "CampusX",
+      image: "https://res.cloudinary.com/dqvwf3z2c/image/upload/v1756399547/Campusx_white_logo_logo_loyl9a.jpg",
+      link: "https://openai.com",
+    custom: "small",
+    },
+    {
+      title: "Associate Sponsor",
+      name: "Geek Room",
+      image: "https://res.cloudinary.com/dqvwf3z2c/image/upload/v1756401930/ed736366-39c2-42d2-8a5e-b055578f1a4c.png",
+      link: "https://openai.com",
+      custom: "",
+    },
+    {
+      title: "Media Partner",
+      name: "DU India",
+      image: "https://res.cloudinary.com/dqvwf3z2c/image/upload/v1756401730/Du_India_1_qmr9s7.jpg",
+      link: "https://openai.com",
+    custom: "small",
+    },
   ],
 
   last: [
-    
   ],
 };
 
